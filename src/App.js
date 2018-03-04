41195400ebe612daf482624c2909617159f0378f
Vadim <vadim11041996@hotmail.com>
2018-03-04 20:22:06 +0200

    can click on put X or O

--- src/App.js
+++ src/App.js
@@ -2,26 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      turn:'X',
+      gameEdned: false,
+      board:Array(9).fill('')
+    }
+  }
+
    clicked(event){
-     console.log(event.target);
+     if(this.state.board[event.target.dataset.square] == ''){
+       this.state.board[event.target.dataset.square] = this.state.turn;
+       event.target.innerText = this.state.turn;
+       this.setState({
+         turn: this.state.turn == 'X' ? 'O' : 'X',
+         board: this.state.board
+       })
+     }
+
+     console.log(this.state.board);
+   }
+
+   checkWinner() {
+
    }
 
   render() {
     return (
       <div id="game">
          <div id="head">
-            Tic Tac Toe Game
+            Tic Tac Game
          </div>
          <div id="board" onClick={(e) => this.clicked(e)}>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
+            <div className="square" data-square="0"></div>
+            <div className="square" data-square="1"></div>
+            <div className="square" data-square="2"></div>
+            <div className="square" data-square="3"></div>
+            <div className="square" data-square="4"></div>
+            <div className="square" data-square="5"></div>
+            <div className="square" data-square="6"></div>
+            <div className="square" data-square="7"></div>
+            <div className="square" data-square="8"></div>
          </div>
 
       </div>

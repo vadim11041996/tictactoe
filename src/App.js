c5131b695a2c22bd994a784f5ee3efd6f63bb92d
Vadim <vadim11041996@hotmail.com>
2018-03-04 20:04:33 +0200

    created game interface

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+   clicked(event){
+     console.log(event.target);
+   }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="game">
+         <div id="head">
+            Tic Tac Toe Game
+         </div>
+         <div id="board" onClick={(e) => this.clicked(e)}>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+         </div>
+
       </div>
     );
   }
